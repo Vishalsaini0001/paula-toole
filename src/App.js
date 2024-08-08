8f667b1f29e2fcb7e79eedc85578c9cffcd5d577
vishal <vishal.saini.99154@gmail.com>
2024-08-08 16:24:43 +0530

    front page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import FrontPage from './pages/FrontPage';
 import PRStrategy from './pages/PRStrategy';
 
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
     <Navbar/>
-    
+    <FrontPage/>
     <PRStrategy/>
     <Footer/>
     
